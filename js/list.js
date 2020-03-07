ba49997705081a06daeb1992dffd4d0d449146a2
Tim Nguyen <ntim.bugs@gmail.com>
2020-03-07 11:47:20 +0000

    Add support for one query per list

---
+++ js/list.js
@@ -0,0 +1,195 @@
+const BUGZILLA_DOMAIN = "https://bugzilla.mozilla.org";
+const USED_FIELDS = [
+  "id",
+  "summary",
+  "status",
+  "resolution",
+  "depends_on",
+  "priority",
+  "type",
+  "component",
+  "product",
+  "keywords",
+  "assigned_to",
+];
+
+customElements.define("bug-list", class List extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot) {
+      return;
+    }
+    this.setAttribute("draggable", "true");
+
+    const template = document.getElementById("list");
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.append(template.content.cloneNode(true));
+
+    this.deleteButton = this.shadowRoot.querySelector(".list-delete");
+    this.deleteButton.addEventListener("click", () => {
+      if (window.confirm(`Do you really want to delete "${this.name}"?`)) {
+        this.remove();
+      }
+    });
+
+    this.editButton = this.shadowRoot.querySelector(".list-edit");
+    this.editButton.addEventListener("click", () => {
+      this.showEditDialog();
+    });
+
+    this.statusText = this.shadowRoot.querySelector(".list-status");
+    this.items = this.shadowRoot.querySelector(".list-items");
+
+    this.addEventListener("dragenter", (ev) => {
+      if (document.querySelector(".dragged")) {
+        this.classList.add("dragover");
+        UI_STATE.lastDropTarget = this;
+      }
+    });
+
+    this.addEventListener("dragleave", (ev) => {
+      this.classList.remove("dragover");
+    });
+
+    this.addEventListener("dragstart", (ev) => {
+      if (!ev.originalTarget.classList.contains("bug")) {
+        this.classList.add("dragged");
+      }
+    });
+
+    this.addEventListener("dragend", (ev) => {
+      this.classList.remove("dragged");
+      const destination = UI_STATE.lastDropTarget;
+      if (destination) {
+        destination.parentNode.insertBefore(this, destination.nextElementSibling);
+        UI_STATE.lastDropTarget = null;
+      }
+    });
+  }
+
+  get name() {
+    return this.dataset.name;
+  }
+
+  set name(newName) {
+    if (this.name == newName) {
+      return;
+    }
+
+    this.shadowRoot.querySelector(".list-name").textContent = newName;
+    return this.dataset.name = newName;
+  }
+
+  get query() {
+    return this.dataset.query;
+  }
+
+  set query(newQuery) {
+    if (this.query == newQuery) {
+      return;
+    }
+
+    this.statusText.textContent = "Loading";
+    this.items.textContent = "";
+
+    this.fetchAndAppendBugs(newQuery).then(() => {
+      this.statusText.textContent = "";
+    });
+    return this.dataset.query = newQuery;
+  }
+
+  async fetchAndAppendBugs(query) {
+    let results;
+    try {
+      results = await getQuickSearchResults(query);
+    } catch (e) {
+      results = [];
+      this.statusText.textContent = e.message;
+      return;
+    }
+
+    if (!results.length) {
+      this.statusText.textContent = "No bugs found";
+    }
+
+    let bugTemplate = document.getElementById("bug-template").content;
+    this.items.append(...results.map(bug => {
+      let element = bugTemplate.cloneNode(true);
+      element.querySelector(".bug").href = BUGZILLA_DOMAIN + "/" + bug.id;
+      element.querySelector(".bug-title").textContent = bug.summary;
+      if (list != "not_started") {
+        element.querySelector(".bug-assignee").textContent = bug.assigned_to_detail.nick || bug.assigned_to_detail.email;
+      }
+      element.querySelector(".bug-type").textContent = bug.type;
+      element.querySelector(".bug-keywords").textContent = bug.keywords.join(", ");
+
+      let compStr = bug.product + " :: " + bug.component;
+      let compElement = element.querySelector(".bug-component");
+      compElement.textContent = compStr;
+
+      let { background: compBackground, textColor: compColor } = hashColor(compStr);
+      compElement.style.backgroundColor = compBackground;
+      compElement.style.color = compColor;
+      return element;
+    }));
+  }
+
+  showEditDialog() {
+    const dialog = document.getElementById("edit-list");
+    const title = document.getElementById("edit-list-title");
+    title.textContent = "Edit list";
+
+    const nameInput = document.getElementById("edit-list-name");
+    nameInput.value = this.name;
+
+    const queryInput = document.getElementById("edit-list-query");
+    queryInput.value = this.query;
+
+    const cancelButton = document.getElementById("edit-list-cancel");
+    cancelButton.onclick = () => dialog.close();
+
+    const form = dialog.querySelector("form");
+    form.onsubmit = e => {
+      this.name = nameInput.value;
+      this.query = queryInput.value;
+      dialog.close();
+      e.preventDefault();
+    };
+    dialog.showModal();
+  }
+});
+
+async function getQuickSearchResults(query) {
+  const json = await fetch(`${BUGZILLA_DOMAIN}/rest/bug?quicksearch=${encodeURIComponent(query)}&include_fields=${USED_FIELDS.join(",")}`)
+    .then(r => r.json());
+  if (json.error) {
+    throw new Error(json.message);
+  }
+  return json.bugs;
+}
+
+function hashCode(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
+
+function hashColor(str) {
+  const i = hashCode(str);
+  const c = (i & 0x00FFFFFF)
+    .toString(16)
+    .toUpperCase();
+  const background = "00000".substring(0, 6 - c.length) + c;
+  const r = parseInt(background.substring(4, 6), 16);
+  const g = parseInt(background.substring(2, 4), 16);
+  const b = parseInt(background.substring(0, 2), 16);
+
+  const isBackgroundDark = (0.2125 * r + 0.7154 * g + 0.0721 * b) <= 110;
+  const textColor = isBackgroundDark ? "#ffffff" : "#000000";
+  return { background: "#" + background, textColor };
+}
--- js/script.js
+++ js/script.js
@@ -1,130 +1,111 @@
-const BUGZILLA_DOMAIN = "https://bugzilla.mozilla.org";
-const USED_FIELDS = [
-  "id",
-  "summary",
-  "status",
-  "resolution",
-  "depends_on",
-  "type",
-  "component",
-  "product",
-  "keywords",
-  "assigned_to",
-];
+window.UI_STATE = {
+  lastDropTarget: null,
+};
 
-function getBug(bug) {
-  return fetch(`${BUGZILLA_DOMAIN}/rest/bug/${bug}?include_fields=${USED_FIELDS.join(",")}`)
-    .then(r => r.json())
-    .then(r => r.bugs[0]);
-}
-
-async function getQuickSearchResults(query) {
-  const json = await fetch(`${BUGZILLA_DOMAIN}/rest/bug?quicksearch=${encodeURIComponent(query)}&include_fields=${USED_FIELDS.join(",")}`)
-    .then(r => r.json());
-  if (json.error) {
-    throw new Error(json.message);
-  }
-  return json.bugs;
-}
-
-async function getBoards(query) {
-  let results = await getQuickSearchResults(query);
-  let boards = {
-    done: [],
-    in_progress: [],
-    not_started: [],
-  };
-  for (let data of results) {
-    if (data.resolution == "FIXED") {
-      boards.done.push(data);
-    } else if (data.status == "ASSIGNED" || !data.assigned_to.includes("nobody")) {
-      boards.in_progress.push(data);
-    } else {
-      boards.not_started.push(data);
-    }
-  }
-  return boards;
-}
-
-async function loadBoard(query) {
+async function loadPreset(preset) {
   document.body.classList.add("loading");
 
-  let boards;
-  try {
-    boards = await getBoards(query);
-  } catch (e) {
-    console.log(e);
-    document.getElementById("status").textContent = e.message;
-    document.body.classList.remove("loading");
-    return;
-  }
-
-  let bugTemplate = document.getElementById("bug-template").content;
-  for (let board in boards) {
-    let container = document.getElementById(board);
-    container.textContent = "";
-    container.append(...boards[board].map(bug => {
-      let element = bugTemplate.cloneNode(true);
-      element.querySelector(".bug").href = BUGZILLA_DOMAIN + "/" + bug.id;
-      element.querySelector(".bug-title").textContent = bug.summary;
-      if (board != "not_started") {
-        element.querySelector(".bug-assignee").textContent = bug.assigned_to_detail.nick || bug.assigned_to_detail.email;
-      }
-      element.querySelector(".bug-type").textContent = bug.type;
-      element.querySelector(".bug-keywords").textContent = bug.keywords.join(", ");
-
-      let compStr = bug.product + " :: " + bug.component;
-      let compElement = element.querySelector(".bug-component");
-      compElement.textContent = compStr;
-
-      let {background: compBackground, textColor: compColor } = hashColor(compStr);
-      compElement.style.backgroundColor = compBackground;
-      compElement.style.color = compColor;
-      return element;
-    }));
+  let lists = document.getElementById("lists");
+  lists.textContent = "";
+  for (let {name, query} of preset) {
+    let list = document.createElement("bug-list");
+    lists.append(list);
+    list.name = name;
+    list.query = query;
   }
   document.body.classList.remove("loading");
 }
 
+function showAddDialog() {
+  const dialog = document.getElementById("edit-list");
+  const title = document.getElementById("edit-list-title");
+  title.textContent = "Add new list";
+
+  const nameInput = document.getElementById("edit-list-name");
+  nameInput.value = "";
+
+  const queryInput = document.getElementById("edit-list-query");
+  queryInput.value = "";
+
+  const cancelButton = document.getElementById("edit-list-cancel");
+  cancelButton.onclick = () => dialog.close();
+
+  const form = dialog.querySelector("form");
+  form.onsubmit = e => {
+    let list = document.createElement("bug-list");
+    document.getElementById("lists").append(list);
+    list.name = nameInput.value;
+    list.query = queryInput.value;
+
+    dialog.close();
+    e.preventDefault();
+  };
+  dialog.showModal();
+}
+
+function getBoardURL() {
+  const searchParams = new URLSearchParams();
+
+  const titleInput = document.getElementById("title");
+  if (titleInput.value) {
+    searchParams.append("title", titleInput.value);
+  }
+  for (const list of document.querySelectorAll("bug-list")) {
+    searchParams.append("name", list.name);
+    searchParams.append("query", list.query);
+  }
+
+  const base = new URL(location.pathname, location.href).href;
+  return base + "?" + searchParams.toString();
+}
+
 (function init() {
-  let queryInput = document.getElementById("query");
-  queryInput.addEventListener("change", () => {
-    loadBoard(queryInput.value);
-  });
+  for (const dialog of document.querySelectorAll("dialog")) {
+    dialogPolyfill.registerDialog(dialog);
+  }
+
+  document.getElementById("list-new").onclick = showAddDialog;
+  const clipboardButton = document.getElementById("list-url");
+  clipboardButton.onclick = async () => {
+    await navigator.clipboard.writeText(getBoardURL());
+    const lastTextContent = clipboardButton.textContent;
+    clipboardButton.textContent = "Copied";
+    clipboardButton.classList.add("copied");
+    await new Promise(r => setTimeout(r, 1000));
+    clipboardButton.textContent = lastTextContent;
+    clipboardButton.classList.remove("copied");
+  };
 
   let searchParams = new URLSearchParams(location.search);
-  let query = searchParams.get("query");
+  let title = searchParams.get("title");
+  let names = searchParams.getAll("name");
+  let queries = searchParams.getAll("query");
+
   let bugId = searchParams.get("id");
-  if (!query && bugId) {
-    query = "ALL blocked:" + bugId;
+  let preset = [];
+
+  if (!queries.length && bugId) {
+    preset = [
+      { name: "Not Started", query: `status:NEW blocked:${bugId}` },
+      { name: "In Progress", query: `status:ASSIGNED blocked:${bugId}` },
+      { name: "Done", query: `resolution:FIXED blocked:${bugId}` },
+    ];
+
+    if (!title) {
+      title = "Bug " + bugId + " dependencies";
+    }
   }
 
-  if (query) {
-    loadBoard(query);
-    queryInput.value = query;
+  if (queries.length) {
+    for (let i = 0; i < queries.length; i++) {
+      let name = names[i] || "Untitled";
+      preset.push({ name, query: queries[i] });
+    }
+  }
+  if (preset.length) {
+    loadPreset(preset);
+  }
+  if (title) {
+    document.getElementById("title").value = title;
   }
 })();
-
-
-function hashCode(str) { // java String#hashCode
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  return hash;
-}
-
-function hashColor(str) {
-  const i = hashCode(str);
-  const c = (i & 0x00FFFFFF)
-    .toString(16)
-    .toUpperCase();
-  const background = "00000".substring(0, 6 - c.length) + c;
-  const r = parseInt(background.substring(4, 6), 16);
-  const g = parseInt(background.substring(2, 4), 16);
-  const b = parseInt(background.substring(0, 2), 16);
-
-  const isBackgroundDark = (0.2125 * r + 0.7154 * g + 0.0721 * b) <= 110;
-  const textColor = isBackgroundDark ? "#ffffff" : "#000000";
-  return { background: "#" + background, textColor };
-}
